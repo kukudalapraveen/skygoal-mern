932318d35d8528fabddfb3ed5b918c5ec0a56e6e
kukudalapraveen <kukudalapraveen@gmail.com>
2024-11-01 10:29:49 +0530

    added home button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Link, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Welcome from './components/Welcome'; 
@@ -7,8 +7,9 @@ import './App.css';
 
 const App = () => {
   const location = useLocation();
-
+  const navigate = useNavigate();
   const isSignupPage = location.pathname === '/signup';
+  const isLoginPage = location.pathname === '/login';
 
   return (
     <div className="background-container">
@@ -16,17 +17,27 @@ const App = () => {
         <div className="welcome-message">
           <h1>Welcome to Our Platform!</h1>
           <p>Discover amazing content and connect with a vibrant community.</p>
-          <p>Happy Diwali!!!!.</p>
-
+          <p>Happy Diwali!!!!</p>
         </div>
+
         <Routes>
-          {/* Redirect from '/' to '/login' by default */}
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/:name" element={<Welcome />} /> {/* Dynamic route here */}
+          <Route path="/:name" element={<Welcome />} /> {/* Dynamic route */}
         </Routes>
 
+        {/* Display Home button on Signup and Login pages */}
+        {(isLoginPage || isSignupPage) && (
+          <button 
+            onClick={() => navigate('/')} 
+            className={`home-button ${isSignupPage ? 'signup-home-button' : ''}`}
+          >
+            Home
+          </button>
+        )}
+
+        {/* Display Signup link unless on the Signup page */}
         {!isSignupPage && (
           <div className="signup-box">
             <p>Don't have an account?</p>
@@ -45,5 +56,55 @@ const AppWrapper = () => (
 );
 
 export default AppWrapper;
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, useLocation, Link } from 'react-router-dom';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Welcome from './components/Welcome'; 
+// import './App.css';
+
+// const App = () => {
+//   const location = useLocation();
+
+//   const isSignupPage = location.pathname === '/signup';
+
+//   return (
+//     <div className="background-container">
+//       <div className="overlay">
+//         <div className="welcome-message">
+//           <h1>Welcome to Our Platform!</h1>
+//           <p>Discover amazing content and connect with a vibrant community.</p>
+//           <p>Happy Diwali!!!!.</p>
+
+//         </div>
+//         <Routes>
+//           {/* Redirect from '/' to '/login' by default */}
+//           <Route path="/" element={<Login />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup />} />
+//           <Route path="/:name" element={<Welcome />} /> {/* Dynamic route here */}
+//         </Routes>
+
+//         {!isSignupPage && (
+//           <div className="signup-box">
+//             <p>Don't have an account?</p>
+//             <Link to="/signup" className="signup-link">Signup</Link>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// const AppWrapper = () => (
+//   <Router>
+//     <App />
+//   </Router>
+// );
+
+// export default AppWrapper;
 
 
